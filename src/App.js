91b9a6630ecd418cbfd60e7861458436cd842016
AAYUSH  SHAKYAWAR <147983388+SourceOfStrengers@users.noreply.github.com>
2025-03-19 08:08:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -8,8 +8,25 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+            <div className='container'>
+            <h1 className='mt-3'>Shopping App</h1>
+                <div className='row mt-3'>
+                  <div className="App">
+                    <CartValue />
+                  </div>
+                  <div className="App">
+                    <ExpenseList />
+                  </div>
+                  <div className="App">
+                    <ItemSelected/>
+                  </div>
+                  <div className="App">
+                    <Location />
+                  </div>
+                </div>
+            </div>
+        </AppProvider>
   );
 }
 
